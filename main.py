508ed643be391d0346c858520c572432aafd9d7c
Viktor Moskalenko <tehb23@gmail.com>
2022-03-11 16:54:08 +0300

    Рабочте фильтры

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
    Получение запросов о фильтре с index.html обработка и формирование
    запросов к БД MySql"""
 
-from pydantic import BaseModel
 import models
 from fastapi import FastAPI, Request, Depends
 from fastapi.templating import Jinja2Templates
@@ -28,13 +27,39 @@ def get_db():
         db.close()
 
 @app.get("/")
-def table(request: Request, db: Session = Depends(get_db)):
+def table(request: Request, db: Session = Depends(get_db), name = None, operation = None, argument = None):
     """
     Отображаем страницу с таблицей / домашнюю страницу
     """
-    products = db.query(Product).all()
+    products = db.query(Product)
+    """
+    output_products = db.query(Product)
+    products = []
+    
+    for i in range (10):
+        products.append(output_products[i])
+    print(len(products))
+    """
 
 
+    if name == '0' and operation == '0':
+        products = products.filter(Product.name == argument)
+    elif name == '0' and operation == '1':
+        products = products.filter(Product.name.like(f"%{argument}%"))
+    elif name == '1' and operation == '0':
+        products = products.filter(Product.quantity == argument)
+    elif name == '1' and operation == '2':
+        products = products.filter(Product.quantity > argument)
+    elif name == '1' and operation == '3':
+        products = products.filter(Product.quantity < argument)
+    elif name == '2' and operation == '0':
+        products = products.filter(Product.distance == argument)
+    elif name == '2' and operation == '2':
+        products = products.filter(Product.distance > argument)
+    elif name == '2' and operation == '3':
+        products = products.filter(Product.distance < argument)   
+
+    
     return templates.TemplateResponse("table.html", {
         "request": request,
         "products": products
